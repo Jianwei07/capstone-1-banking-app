f66b27a96efa9a9133264bb15ff29132ec67f154
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-08-17 16:29:07 +0800

    Update Profile.js

--- capstone-1-team3/src/components/Profile.js
+++ capstone-1-team3/src/components/Profile.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
 
-export default function Profile() {
+export function Profile() {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [nric, setNric] = useState('')
