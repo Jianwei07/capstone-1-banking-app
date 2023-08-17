9885abe4b6c0e4c886c7377afe0814278e467dc5
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-08-17 14:51:13 +0800

    Update SignIn.js

---
+++ capstone-1-team3/src/components/SignIn.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const SignIn = () => {
+    return (
+        <div>
+            <form>
+                <h2>Sign In</h2>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <input type="checkbox" id='rememberMe' />
+                <label htmlFor='rememberMe'>Remember Me</label>
+                {isWrongLogin ? <p>The email or password that you have entered is incorrect</p> : null}
+                <button type='submit' onClick={handleSubmit}>Sign In</button>
+            </form>
+            <a href='/registration'>Sign Up</a>
+        </div>
+    )
+}
