40512c4509195bc8dd4df42fd51a260f29773313
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-08-17 15:30:25 +0800

    Added submit function for login

--- capstone-1-team3/src/components/SignIn.js
+++ capstone-1-team3/src/components/SignIn.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
-export const SignIn = () => {
-    const handleSubmit = () =>{
+export const SignIn = (props) => {
+    const [loginInput, setLoginInput] = useState({ email: "", password: "" })
+    const [isWrongLogin, setIsWrongLogin] = useState(false)
+    const navigate = useNavigate();
+    const { profileList } = props
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const profileChecker = profileList.find(profile => profile.email === loginInput.email) //find and return the profile for the input email
+        if (profileChecker && profileChecker.password === loginInput.password) {
+            //correct information
+            setIsWrongLogin(false);
+            navigate('/landingpage');
+        } else {
+            //wrong information
+            setIsWrongLogin(true);
+            setLoginInput(prevInput => ({ ...prevInput, password: "" }));
+        }
         
     }
     return (
         <div>
+            <h1>InnovPay</h1>
             <form>
                 <h2>Sign In</h2>
-                <input type="email" placeholder="Email" />
-                <input type="password" placeholder="Password" />
-                <input type="checkbox" id='rememberMe' />
+                <input type="email" value="" placeholder="Email" />
+                <input type="password" value="" placeholder="Password" />
+                <input type="checkbox" id='rememberMe' value="false" />
                 <label htmlFor='rememberMe'>Remember Me</label>
-                {/* {isWrongLogin ? <p>The email or password that you have entered is incorrect</p> : null} */}
-                <button type='submit' onClick={handleSubmit}>Sign In</button>
+                {isWrongLogin ? <p>The email or password that you have entered is incorrect</p> : null}
+                <button type='submit' onClick={(e) => handleSubmit(e)}>Sign In</button>
             </form>
             <a href='/registration'>Sign Up</a>
         </div>
