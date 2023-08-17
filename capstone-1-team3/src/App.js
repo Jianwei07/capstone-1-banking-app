883185870a67301489bcffe6753ee1ed15063c2d
kacey152 <110165967+kacey152@users.noreply.github.com>
2023-08-17 16:12:08 +0800

    Updated routing for profile and registration

--- capstone-1-team3/src/App.js
+++ capstone-1-team3/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { SignIn } from './components/SignIn'
 import { LandingPage } from './components/LandingPage';
+import { Registration } from './components/Registration'
+import { Profile } from './components/Profile'
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -21,6 +23,8 @@ function App() {
         <Routes>
           <Route path='/' element={<SignIn profileList={profileList}/>} />
           <Route path='/landingpage' element={<LandingPage />} />
+          <Route path='/registration' element={<Registration />} />
+          <Route path='/profile' element={<Profile />} />
         </Routes>
       </Router>
       
