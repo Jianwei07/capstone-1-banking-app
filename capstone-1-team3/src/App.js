62d2a0d2f59b52304a14852278de34a1517f91c8
jayden liaw <liawjianwei@outlook.com>
2023-08-17 16:26:41 +0800

    Adding LandingPage.js
    Updating LandingPage.js - Base File

--- capstone-1-team3/src/App.js
+++ capstone-1-team3/src/App.js
@@ -1,30 +1,30 @@
-import './App.css';
-import { SignIn } from './components/SignIn'
-import { LandingPage } from './components/LandingPage';
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { SignIn } from "./components/SignIn";
+import { LandingPage } from "./components/LandingPage";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [profileList, setProfile] = useState([{
-    email: "",
-    password: "",
-    fName: "",
-    lName: "",
-    nRIC: "",
-    address: "",
-    birthDate: "",
-    contactNumber: "",
-  }])
+  const [profileList, setProfile] = useState([
+    {
+      email: "",
+      password: "",
+      fName: "",
+      lName: "",
+      nRIC: "",
+      address: "",
+      birthDate: "",
+      contactNumber: "",
+    },
+  ]);
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<SignIn profileList={profileList}/>} />
-          <Route path='/landingpage' element={<LandingPage />} />
+          <Route path="/landingpage" element={<LandingPage />} />
+          <Route path="/" element={<SignIn profileList={profileList} />} />
         </Routes>
       </Router>
-      
-
     </div>
   );
 }
