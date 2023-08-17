90ce2da0b29df4ee7a416b5f6dee3adddf938df7
Justin Lee <jlcw99@gmail.com>
2023-08-17 19:22:18 +0800

    css

--- capstone-1-team3/src/components/Registration.js
+++ capstone-1-team3/src/components/Registration.js
@@ -4,35 +4,48 @@ import './Registration.css'
 function Registration() {
     return (
         <div className='registration'>
-            <h1 className='title'>Register for a Bank Account</h1>
             <form className='container'>
-                <label htmlFor='email'>Email: </label>
-                <input type='email' id='email'></input>
-                <br></br>
-                <label htmlFor='password'>Password: </label>
-                <input type='password' id='password'></input>
-                <br></br>
-                <label htmlFor='rePassword'>Re-enter Password: </label>
-                <input type='password' id='rePassword'></input>
-                <br></br>
-                <label htmlFor='first'>First Name: </label>
-                <input type='text' id='first'></input>
-                <label htmlFor='last'>Last Name: </label>
-                <input type='text' id='last'></input>
-                <br></br>
-                <label htmlFor='nric'>NRIC: </label>
-                <input type='text' id='nric'></input>
-                <br></br>
-                <label htmlFor='address'>Address: </label>
-                <input type='text' id='address'></input>
-                <br></br>
-                <label htmlFor='dob'>Date of Birth: </label>
-                <input type='date' id='dob'></input>
-                <br></br>
-                <label htmlFor='contact'>Contact Number: </label>
-                <input type='tel' id='contact'></input>
-                <br></br>
-                <input type='submit'></input>
+            <h1 className='title'>Register for a Bank Account</h1>
+            <br />
+                <div className='input-group'>
+                    <label htmlFor='email'>Email:</label>
+                    <input type='email' id='email' />
+                </div>
+                <div className='input-group'>
+                    <label htmlFor='password'>Password:</label>
+                    <input type='password' id='password' />
+                </div>
+                <div className='input-group'>
+                    <label htmlFor='rePassword'>Re-enter Password:</label>
+                    <input type='password' id='rePassword' />
+                </div>
+                <div className='name-group'>
+                    <div className='input-group'>
+                        <label htmlFor='first'>First Name:</label>
+                        <input type='text' id='first' />
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor='last'>Last Name:</label>
+                        <input type='text' id='last' />
+                    </div>
+                </div>
+                <div className='input-group'>
+                    <label htmlFor='nric'>NRIC:</label>
+                    <input type='text' id='nric' />
+                </div>
+                <div className='input-group'>
+                    <label htmlFor='address'>Address:</label>
+                    <input type='text' id='address' />
+                </div>
+                <div className='input-group'>
+                    <label htmlFor='dob'>Date of Birth:</label>
+                    <input type='date' id='dob' />
+                </div>
+                <div className='input-group'>
+                    <label htmlFor='contact'>Contact Number:</label>
+                    <input type='tel' id='contact' />
+                </div>
+                <button type='submit'>Submit</button>
             </form>
         </div>
     );
