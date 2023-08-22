86db49cbce859c8aac4cfc40444fb223d9c536fd
Justin Lee <jlcw99@gmail.com>
2023-08-22 10:53:05 +0800

    contact

---
+++ capstone-1-team3/src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+
+function Contact() {
+  return (
+    <div>InnovPay: Where Security, Innovation, and Prosperity Converge for You.
+        <div>Step into the future of finance within a transparent community. 
+Secure, diverse, and empowering solutions await your journey.</div>
+    </div>
+  )
+}
+
+export default Contact
