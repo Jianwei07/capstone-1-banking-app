dbdfcfb7a7c5f40b01544ef3ac428484274c16dd
jayden liaw <liawjianwei@outlook.com>
2023-08-22 14:56:44 +0800

    Added icon MUI
    pls npm install

--- capstone-1-team3/src/components/Hero.js
+++ capstone-1-team3/src/components/Hero.js
@@ -2,6 +2,9 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import { Typography, Button } from "@mui/material";
+import SavingsIcon from "@mui/icons-material/Savings";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
 import iPhone from "../style/iPhone.png";
 import { useNavigate } from "react-router-dom";
 
@@ -12,10 +15,11 @@ const Hero = () => {
       <Grid
         container
         spacing={0.5}
-        padding="100px 0px 20px 20px"
+        padding="60px 120px 10px 20px"
         sx={{ backgroundColor: "#2D2D2D" }}
         id="Hero"
       >
+        {/* Left Content */}
         <Grid
           item
           xs={12}
@@ -23,41 +27,43 @@ const Hero = () => {
           sx={{
             display: "flex",
             flexDirection: "column",
-            justifyContent: "center",
+            justifyContent: "space-between",
           }}
         >
-          <Typography
-            variant="h1"
-            align="left"
-            color="white"
-            sx={{
-              fontFamily: "Open Sans",
-              fontSize: "64px",
-              fontWeight: 700,
-              width: "768px",
-            }}
-          >
-            Bringing Borders Closer with{" "}
-            <span style={{ color: "#F8F3A3" }}>Innov</span>Pay
-          </Typography>
-          <Typography
-            color="white"
-            variant="subtitle1"
-            align="left"
-            paragraph
-            sx={{
-              fontFamily: "Open Sans",
-              fontWeight: 300,
-              fontSize: "32px",
-              width: "768px",
-              height: "230px",
-            }}
-          >
-            InnovPay revolutionizes finance, uniting interest generation and
-            currency exchange within a fortified multi-currency digital wallet,
-            expanding your financial horizons.
-          </Typography>
           <div>
+            <Typography
+              variant="h1"
+              align="left"
+              color="white"
+              sx={{
+                fontFamily: "Open Sans",
+                fontSize: "64px",
+                fontWeight: 700,
+                marginBottom: "10px",
+              }}
+            >
+              Bringing Borders Closer with{""}
+              <span style={{ color: "#F8F3A3" }}> Innov</span>Pay
+            </Typography>
+            <Typography
+              color="white"
+              variant="subtitle1"
+              align="left"
+              paragraph
+              sx={{
+                fontFamily: "Open Sans",
+                fontWeight: 300,
+                fontSize: "28px",
+                width: "768px",
+                height: "230px",
+                marginBottom: "0px",
+              }}
+            >
+              <span style={{ color: "#F8F3A3" }}> Innov</span>Pay revolutionizes
+              finance, uniting interest generation and currency exchange within
+              a fortified multi-currency digital wallet, expanding your
+              financial horizons.
+            </Typography>
             <Button
               color="inherit"
               onClick={() => navigate("/registration")}
@@ -93,16 +99,43 @@ const Hero = () => {
             >
               EXCHANGE RATES
             </Button>
+            {/* icons to preview features */}
+            <Grid container spacing={3} justifyContent="left">
+              <Grid item xs={12} sm={4} md={4}>
+                <CurrencyExchangeIcon size={640} color="#e3f2fd" />
+                <Typography color="white" variant="h6" gutterBottom>
+                  Competitive Exchange Rates (42 Currencies)
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={4} md={4}>
+                <AccountBalanceWalletIcon size={640} color="#e3f2fd" />
+                <Typography color="white" variant="h6" gutterBottom>
+                  Secure Multi-Currency Wallet
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={4} md={4}>
+                <SavingsIcon size={640} color="#e3f2fd" />
+                <Typography color="white" variant="h6" gutterBottom>
+                  Earn 3.28% Interest
+                </Typography>
+                <Typography color="#white" variant="h10" gutterBottom>
+                  T&C applies*
+                </Typography>
+              </Grid>
+            </Grid>
+            {/* icons to preview features */}
           </div>
         </Grid>
+
+        {/* Right Content */}
         <Grid
           item
           xs={12}
           md={6}
           sx={{
             display: "flex",
-            justifyContent: "right",
-            alignItems: "right",
+            justifyContent: "flex-end",
+            alignItems: "center",
           }}
         >
           <img src={iPhone} width="65%" alt="iPhone" />
