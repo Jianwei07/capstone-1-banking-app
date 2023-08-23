4d85736aed5f04192dee48774c9deeba70613f5b
jayden liaw <liawjianwei@outlook.com>
2023-08-23 12:03:51 +0800

    Added GetinTouch.js
    Get In Touch.js

---
+++ capstone-1-team3/src/components/Get-intouch.js
@@ -0,0 +1,178 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Typography,
+  TextField,
+  TextareaAutosize,
+  Grid,
+} from "@mui/material";
+
+const GetInTouch = () => {
+  return (
+    <Box
+      sx={{
+        minHeight: "100vh",
+      }}
+      id="Getintouch"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      width="100%"
+      padding={{ xs: "0px 20px", md: "0px 100px" }}
+      bgcolor="#152840"
+    >
+      <Grid
+        container
+        spacing={2}
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid item xs={12} md={6}>
+          <div>
+            <Typography
+              variant="h1"
+              align="left"
+              color="white"
+              sx={{
+                fontFamily: "Open Sans",
+                fontSize: "64px",
+                fontWeight: 700,
+                marginBottom: "10px",
+              }}
+            >
+              <span style={{ color: "#F8F3A3" }}>Contact</span> us
+            </Typography>
+            <Typography
+              color="white"
+              variant="body1"
+              align="left"
+              paragraph
+              sx={{
+                fontFamily: "Open Sans",
+                fontWeight: 300,
+                fontSize: "28px",
+                width: "100%",
+                marginBottom: "0px",
+              }}
+            >
+              Need to get in touch with us? Either fill out the form with your
+              personal inquiry or find the support email you’d like to contact
+              below.
+            </Typography>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box
+            width="500px"
+            height="500px"
+            border="1px solid #000"
+            background="#152840"
+            boxShadow="0px 4px 4px 0px rgba(0, 0, 0, 0.25)"
+            borderRadius="10px"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            padding="20px"
+          >
+            <form style={{ width: "100%", padding: "20px" }}>
+              <div
+                style={{
+                  marginBottom: "10px",
+                  display: "flex",
+                  gap: "32px",
+                }}
+              >
+                <div style={{ width: "240px" }}>
+                  <Typography variant="body1" color="white">
+                    First Name:
+                  </Typography>
+                  <TextField
+                    fullWidth
+                    variant="outlined"
+                    margin="dense"
+                    sx={{ backgroundColor: "#47546C", borderRadius: "10px" }}
+                    inputProps={{ style: { color: "#F8F3A3" } }}
+                  />
+                </div>
+                <div style={{ width: "150px" }}>
+                  <Typography variant="body1" color="white">
+                    Last Name:
+                  </Typography>
+                  <TextField
+                    fullWidth
+                    variant="outlined"
+                    margin="dense"
+                    sx={{ backgroundColor: "#47546C", borderRadius: "10px" }}
+                    inputProps={{ style: { color: "#F8F3A3" } }}
+                  />
+                </div>
+              </div>
+              <div style={{ marginBottom: "10px", width: "422px" }}>
+                <Typography variant="body1" color="white">
+                  Email:
+                </Typography>
+                <TextField
+                  fullWidth
+                  variant="outlined"
+                  margin="dense"
+                  sx={{
+                    backgroundColor: "#47546C",
+                    borderRadius: "10px",
+                  }}
+                  inputProps={{ style: { color: "#F8F3A3" } }}
+                />
+              </div>
+              <div style={{ marginBottom: "10px" }}>
+                <Typography variant="body1" color="white">
+                  What can we help you with?:
+                </Typography>
+                <TextareaAutosize
+                  variant="outlined"
+                  margin="dense"
+                  style={{
+                    backgroundColor: "#47546C",
+                    width: "422px",
+                    height: "150px",
+                    borderRadius: "10px",
+                    resize: "none",
+                    padding: "10px",
+                    border: "none",
+                    color: "#F8F3A3",
+                  }}
+                  rows={4}
+                />
+              </div>
+
+              <Button
+                fullWidth
+                variant="contained"
+                sx={{
+                  width: "100px",
+                  height: "40px",
+                  marginTop: "10px",
+                  backgroundColor: "#F8F3A3",
+                  color: "black",
+                  fontFamily: "Open Sans",
+                  fontWeight: "bold",
+                  borderRadius: "10px",
+                  "&:hover": {
+                    backgroundColor: "white",
+                  },
+                  "&:focus": {
+                    outline: "none",
+                  },
+                }}
+              >
+                Submit
+              </Button>
+            </form>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default GetInTouch;
--- capstone-1-team3/src/components/MuiSignUp.js
+++ capstone-1-team3/src/components/MuiSignUp.js
@@ -1,37 +1,40 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { useNavigate } from 'react-router-dom';
-// Link, 
-import { useForm } from 'react-hook-form';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+// Link,
+import { useForm } from "react-hook-form";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-import ReactPhoneInput from 'react-phone-input-material-ui';
-
-
+import ReactPhoneInput from "react-phone-input-material-ui";
 
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
         Your Website
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -41,56 +44,61 @@ function Copyright(props) {
 const defaultTheme = createTheme();
 
 export default function SignUp(props) {
-
   const navigate = useNavigate();
-  const { register, getValues, formState: { errors }, reset, handleSubmit } = useForm();
+  const {
+    register,
+    getValues,
+    formState: { errors },
+    reset,
+    handleSubmit,
+  } = useForm();
 
   const validation = {
     email: {
       required: {
         value: true,
-        message: 'Email is required.'
-      }
+        message: "Email is required.",
+      },
     },
     password: {
       required: {
         value: true,
-        message: 'Password is required.'
+        message: "Password is required.",
       },
       pattern: {
         value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
-        message: 'Password must have at least 8 characters. It must contain at least a number, a lowercase letter and an uppercase letter.'
-      }
+        message:
+          "Password must have at least 8 characters. It must contain at least a number, a lowercase letter and an uppercase letter.",
+      },
     },
     rePassword: {
       required: {
         value: true,
-        message: 'Re-enter your password.'
+        message: "Re-enter your password.",
       },
-      validate: (value) => value === getValues('password') || 'This does not match the password that you have entered.'
+      validate: (value) =>
+        value === getValues("password") ||
+        "This does not match the password that you have entered.",
     },
     first: {
-      required: false
+      required: false,
     },
     last: {
-      required: false
+      required: false,
     },
     nric: {
-      required: false
+      required: false,
     },
     address: {
-      required: false
+      required: false,
     },
     dob: {
-      required: false
+      required: false,
     },
     contact: {
-      required: false
-    }
+      required: false,
+    },
   };
-
-
-
 
   const OnSubmit = (event) => {
     // event.preventDefault();
@@ -102,10 +110,11 @@ export default function SignUp(props) {
     const data = new FormData(event.currentTarget);
     props.handleRegister(data);
     reset();
-    alert('Your registration is successful! You will be redirected to login now.');
-    navigate('/login');
+    alert(
+      "Your registration is successful! You will be redirected to login now."
+    );
+    navigate("/login");
   };
-
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -114,18 +123,23 @@ export default function SignUp(props) {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5" color="primary">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit(OnSubmit)} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit(OnSubmit)}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -136,7 +150,7 @@ export default function SignUp(props) {
                   id="firstName"
                   label="First Name"
                   autoFocus
-                  {...register('first', validation.first)}
+                  {...register("first", validation.first)}
                 />
                 {errors.first && <p>{errors.first?.message}</p>}
               </Grid>
@@ -148,7 +162,7 @@ export default function SignUp(props) {
                   label="Last Name"
                   name="lastName"
                   autoComplete="family-name"
-                  {...register('last', validation.last)}
+                  {...register("last", validation.last)}
                 />
                 {errors.last && <p>{errors.last?.message}</p>}
               </Grid>
@@ -160,7 +174,7 @@ export default function SignUp(props) {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
-                  {...register('email', validation.email)}
+                  {...register("email", validation.email)}
                 />
                 {errors.email && <p>{errors.email?.message}</p>}
               </Grid>
@@ -173,20 +187,17 @@ export default function SignUp(props) {
                   label="NRIC"
                   name="nric"
                   autoComplete="NRIC"
-                  {...register('nric', validation.nric)}
+                  {...register("nric", validation.nric)}
                 />
                 {errors.nric && <p>{errors.nric?.message}</p>}
               </Grid>
 
-              
-
               <Grid item xs={12} sm={6}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-
-                  <DatePicker label="Date of Birth"
-
-                    {...register('dob', validation.dob)} />
-                  
+                  <DatePicker
+                    label="Date of Birth"
+                    {...register("dob", validation.dob)}
+                  />
                 </LocalizationProvider>
                 {errors.dob && <p>{errors.dob?.message}</p>}
               </Grid>
@@ -199,19 +210,17 @@ export default function SignUp(props) {
                   label="Address"
                   name="Address"
                   autoComplete="Address"
-                  {...register('address', validation.address)}
+                  {...register("address", validation.address)}
                 />
                 {errors.address && <p>{errors.address?.message}</p>}
               </Grid>
 
               <Grid item xs={12} sm={6}>
-                
                 <ReactPhoneInput
-                  defaultCountry={'sg'}
+                  defaultCountry={"sg"}
                   component={TextField}
-                  id='contact'
-                  {...register('contact', validation.contact)}
-
+                  id="contact"
+                  {...register("contact", validation.contact)}
                 />
                 {errors.contact && <p>{errors.contact?.message}</p>}
               </Grid>
@@ -225,7 +234,7 @@ export default function SignUp(props) {
                   type="password"
                   id="password"
                   autoComplete="new-password"
-                  {...register('password', validation.password)}
+                  {...register("password", validation.password)}
                 />
                 {errors.password && <p>{errors.password?.message}</p>}
               </Grid>
@@ -238,13 +247,15 @@ export default function SignUp(props) {
                   type="password"
                   id="repassword"
                   autoComplete="re-enter-password"
-                  {...register('rePassword', validation.rePassword)}
+                  {...register("rePassword", validation.rePassword)}
                 />
                 {errors.rePassword && <p>{errors.rePassword?.message}</p>}
               </Grid>
               <Grid item xs={12}>
                 <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
                   label="I want to receive inspiration, marketing promotions and updates via email."
                 />
               </Grid>
@@ -259,7 +270,7 @@ export default function SignUp(props) {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link to={'/login'} variant="body2">
+                <Link to={"/login"} variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
