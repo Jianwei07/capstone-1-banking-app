07e8e6349636b81ae23bdf1dcb6cb2806f4b5612
jayden liaw <liawjianwei@outlook.com>
2023-08-24 09:08:20 +0800

    Removed copyright and adjusted vh for sign up page

--- capstone-1-team3/src/components/MuiSignUp.js
+++ capstone-1-team3/src/components/MuiSignUp.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Container from '@mui/material/Container';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import ReactPhoneInput from 'react-phone-input-material-ui';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import ReactPhoneInput from "react-phone-input-material-ui";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
 
 const defaultTheme = createTheme();
 
 export default function SignUp(props) {
   const navigate = useNavigate();
-  const {register, getValues, formState: { errors }, reset, handleSubmit} = useForm();
+  const {
+    register,
+    getValues,
+    formState: { errors },
+    reset,
+    handleSubmit,
+  } = useForm();
 
   const validation = {
     email: {
@@ -38,7 +44,8 @@ export default function SignUp(props) {
       },
       pattern: {
         value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
-        message: "Password must have at least 8 characters. It must contain at least a number, a lowercase letter and an uppercase letter.",
+        message:
+          "Password must have at least 8 characters. It must contain at least a number, a lowercase letter and an uppercase letter.",
       },
     },
     rePassword: {
@@ -46,18 +53,20 @@ export default function SignUp(props) {
         value: true,
         message: "Re-enter your password.",
       },
-      validate: (value) => value === getValues("password") || "This does not match the password that you have entered.",
+      validate: (value) =>
+        value === getValues("password") ||
+        "This does not match the password that you have entered.",
     },
     first: {
       required: {
         value: true,
-        message: 'First name is required.'
+        message: "First name is required.",
       },
     },
     last: {
       required: {
         value: true,
-        message: 'Last name is required.'
+        message: "Last name is required.",
       },
     },
     nric: {
@@ -83,7 +92,9 @@ export default function SignUp(props) {
     // });
     props.handleRegister(data);
     reset();
-    alert("Your registration is successful! You will be redirected to login now.");
+    alert(
+      "Your registration is successful! You will be redirected to login now."
+    );
     navigate("/login");
   };
 
@@ -91,52 +102,146 @@ export default function SignUp(props) {
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Box sx={{marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-          <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+        <Box
+          sx={{
+            minHeight: "85vh",
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
-          <Typography component="h1" variant="h5" color="primary">Sign up</Typography>
-          <Box component="form" onSubmit={handleSubmit(onSubmit)} sx={{mt: 3}}>
+          <Typography component="h1" variant="h5" color="primary">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit(onSubmit)}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
-                <TextField type='text' id="first" label="First Name" name="first" fullWidth autoFocus {...register('first', validation.first)} error={errors.first} helperText={errors.first?.message}/>
+                <TextField
+                  type="text"
+                  id="first"
+                  label="First Name"
+                  name="first"
+                  fullWidth
+                  autoFocus
+                  {...register("first", validation.first)}
+                  error={errors.first}
+                  helperText={errors.first?.message}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField type='text' id="last" label="Last Name" name="last" fullWidth {...register('last', validation.last)} error={errors.last} helperText={errors.last?.message}/>
+                <TextField
+                  type="text"
+                  id="last"
+                  label="Last Name"
+                  name="last"
+                  fullWidth
+                  {...register("last", validation.last)}
+                  error={errors.last}
+                  helperText={errors.last?.message}
+                />
               </Grid>
               <Grid item xs={12}>
-                <TextField type='email' id="email" label="Email Address" name="email" fullWidth {...register('email', validation.email)} error={errors.email} helperText={errors.email?.message}/>
+                <TextField
+                  type="email"
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  fullWidth
+                  {...register("email", validation.email)}
+                  error={errors.email}
+                  helperText={errors.email?.message}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField type='text' id="nric" label="NRIC" name="nric" fullWidth {...register('nric', validation.nric)} error={errors.nric} helperText={errors.nric?.message}/>
+                <TextField
+                  type="text"
+                  id="nric"
+                  label="NRIC"
+                  name="nric"
+                  fullWidth
+                  {...register("nric", validation.nric)}
+                  error={errors.nric}
+                  helperText={errors.nric?.message}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker label="Date of Birth" disableFuture/>
+                  <DatePicker label="Date of Birth" disableFuture />
                 </LocalizationProvider>
                 {errors.dob && <p>{errors.dob?.message}</p>}
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField type='text' id="address" label="Address" name="address" fullWidth {...register('address', validation.address)} error={errors.address} helperText={errors.address?.message}/>
+                <TextField
+                  type="text"
+                  id="address"
+                  label="Address"
+                  name="address"
+                  fullWidth
+                  {...register("address", validation.address)}
+                  error={errors.address}
+                  helperText={errors.address?.message}
+                />
               </Grid>
               <Grid item xs={12} sm={6}>
-                <ReactPhoneInput defaultCountry={'sg'} component={TextField} id='contact'/>
+                <ReactPhoneInput
+                  defaultCountry={"sg"}
+                  component={TextField}
+                  id="contact"
+                />
                 {errors.contact && <p>{errors.contact?.message}</p>}
               </Grid>
               <Grid item xs={12}>
-                <TextField type="password" id="password" label="Password" name="password" fullWidth {...register('password', validation.password)} error={errors.password} helperText={errors.password?.message}/>
+                <TextField
+                  type="password"
+                  id="password"
+                  label="Password"
+                  name="password"
+                  fullWidth
+                  {...register("password", validation.password)}
+                  error={errors.password}
+                  helperText={errors.password?.message}
+                />
               </Grid>
               <Grid item xs={12}>
-                <TextField type="password" id="rePassword" label="Re-enter Password" name="rePassword" fullWidth {...register('rePassword', validation.rePassword)} error={errors.rePassword} helperText={errors.rePassword?.message}/>
+                <TextField
+                  type="password"
+                  id="rePassword"
+                  label="Re-enter Password"
+                  name="rePassword"
+                  fullWidth
+                  {...register("rePassword", validation.rePassword)}
+                  error={errors.rePassword}
+                  helperText={errors.rePassword?.message}
+                />
               </Grid>
               <Grid item xs={12}>
-                <FormControlLabel control={<Checkbox value="allowExtraEmails" color="primary" />} label="I want to receive inspiration, marketing promotions and updates via email."/>
+                <FormControlLabel
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
               </Grid>
             </Grid>
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Sign Up</Button>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link to={'/login'}>Already have an account? Sign in</Link>
+                <Link to={"/login"}>Already have an account? Sign in</Link>
               </Grid>
             </Grid>
           </Box>
--- capstone-1-team3/src/components/Profile.js
+++ capstone-1-team3/src/components/Profile.js
@@ -1,76 +1,100 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../style/Profile.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../style/Profile.css";
 
 export function Profile(props) {
-    const navigate = useNavigate()
-    const { currentProfile, handleProfileUpdate, setIsLoggedIn } = props;
-    const [inputProfile, setInputProfile] = useState(currentProfile); //{ email:[currentEmail], profile: profileList[currentEmail] }
+  const navigate = useNavigate();
+  const { currentProfile, handleProfileUpdate, setIsLoggedIn } = props;
+  const [inputProfile, setInputProfile] = useState(currentProfile); //{ email:[currentEmail], profile: profileList[currentEmail] }
 
-    const handleInput = (e) => {
-        e.preventDefault();
-        // setInputProfile({ ...inputProfile, profile: {...inputProfile.profile, [e.target.name]: e.target.value }});
-        setInputProfile({ ...inputProfile, profile: {...inputProfile.profile, [e.target.name]: e.target.value }});
-    };
+  const handleInput = (e) => {
+    e.preventDefault();
+    // setInputProfile({ ...inputProfile, profile: {...inputProfile.profile, [e.target.name]: e.target.value }});
+    setInputProfile({
+      ...inputProfile,
+      profile: { ...inputProfile.profile, [e.target.name]: e.target.value },
+    });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        handleProfileUpdate(inputProfile);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleProfileUpdate(inputProfile);
+  };
 
-    const handleLogOut = () =>{
-        navigate("/");
-        setIsLoggedIn(false)
-        alert("Thank you for banking with us! You are now logged out");
-    }
-    return (
-        <div className="profile-page">
-            <form className="profile-form" onSubmit={handleSubmit}>
-                <h2 className='yourprofile'>Your Profile</h2>
-                <div className='name-group'>
-                    <div className="profile-field">
-                        <label>First Name:</label>
-                        <input type="text" name="fName" value={inputProfile.profile.fName} onChange={(e) => handleInput(e)} />
-                    </div>
-                    <div className="profile-field">
-                        <label>Last Name:</label>
-                        <input type="text" name="lName" value={inputProfile.profile.lName} onChange={(e) => handleInput(e)} />
-                    </div>
-                </div>
-                <div className="profile-field">
-                    <label>NRIC:</label>
-                    <input type="text" name="nRIC" value={inputProfile.profile.nRIC} onChange={(e) => handleInput(e)} />
-                </div>
-                <div className="profile-field">
-                    <label>Address:</label>
-                    <input type="text" name="address" value={inputProfile.profile.address} onChange={(e) => handleInput(e)} />
-                </div>
-                <div className="profile-field">
-                    <label>Date of Birth:</label>
-                    <input
-                        type="date"
-                        name="birthDate"
-                        value={inputProfile.profile.birthDate}
-                        onChange={(e) => handleInput(e)}
-                    />
-                </div>
-                <div className="profile-field">
-                    <label>Contact Number:</label>
-                    <input
-                        type="tel"
-                        name="contactNumber"
-                        value={inputProfile.profile.contactNumber}
-                        onChange={(e) => handleInput(e)}
-                    />
-                </div>
-                <div className='buttons'>
-                    <button type="submit" onClick={handleSubmit}>Update</button>
-                    <button onClick={handleLogOut}>Log Out</button>
-                </div>
-
-            </form>
+  const handleLogOut = () => {
+    navigate("/");
+    setIsLoggedIn(false);
+    alert("Thank you for banking with us! You are now logged out");
+  };
+  return (
+    <div className="profile-page">
+      <form className="profile-form" onSubmit={handleSubmit}>
+        <h2 className="yourprofile">Your Profile</h2>
+        <div className="name-group">
+          <div className="profile-field">
+            <label>First Name:</label>
+            <input
+              type="text"
+              name="fName"
+              value={inputProfile.profile.fName}
+              onChange={(e) => handleInput(e)}
+            />
+          </div>
+          <div className="profile-field">
+            <label>Last Name:</label>
+            <input
+              type="text"
+              name="lName"
+              value={inputProfile.profile.lName}
+              onChange={(e) => handleInput(e)}
+            />
+          </div>
         </div>
-    );
+        <div className="profile-field">
+          <label>NRIC:</label>
+          <input
+            type="text"
+            name="nRIC"
+            value={inputProfile.profile.nRIC}
+            onChange={(e) => handleInput(e)}
+          />
+        </div>
+        <div className="profile-field">
+          <label>Address:</label>
+          <input
+            type="text"
+            name="address"
+            value={inputProfile.profile.address}
+            onChange={(e) => handleInput(e)}
+          />
+        </div>
+        <div className="profile-field">
+          <label>Date of Birth:</label>
+          <input
+            type="date"
+            name="birthDate"
+            value={inputProfile.profile.birthDate}
+            onChange={(e) => handleInput(e)}
+          />
+        </div>
+        <div className="profile-field">
+          <label>Contact Number:</label>
+          <input
+            type="tel"
+            name="contactNumber"
+            value={inputProfile.profile.contactNumber}
+            onChange={(e) => handleInput(e)}
+          />
+        </div>
+        <div className="buttons">
+          <button type="submit" onClick={handleSubmit}>
+            Update
+          </button>
+          <button onClick={handleLogOut}>Log Out</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Profile;
